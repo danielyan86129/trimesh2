9a4a65731f1c92c0a20fd4a626418b99b8b9745e
yajieyan <yajie_yan@apple.com>
2022-11-20 14:46:19 -0500

    - make targets public - add prefix for lib - modify other cpp files to use prefix accordingly

--- libsrc/faceflip.cc
+++ libsrc/faceflip.cc
@@ -6,29 +6,27 @@ faceflip.cc
 Flip the order of vertices in each face.  Turns the mesh inside out.
 */
 
-#include "TriMesh.h"
-#include "TriMesh_algo.h"
+#include "trimesh/TriMesh.h"
+#include "trimesh/TriMesh_algo.h"
 #define dprintf TriMesh::dprintf
 using namespace std;
 
-
 namespace trimesh {
 
-void faceflip(TriMesh *mesh)
-{
-	bool had_tstrips = !mesh->tstrips.empty();
-	mesh->need_faces();
-	mesh->tstrips.clear();
+void faceflip(TriMesh *mesh) {
+  bool had_tstrips = !mesh->tstrips.empty();
+  mesh->need_faces();
+  mesh->tstrips.clear();
 
-	dprintf("Flipping faces... ");
-	int nf = mesh->faces.size();
+  dprintf("Flipping faces... ");
+  int nf = mesh->faces.size();
 #pragma omp parallel for
-	for (int i = 0; i < nf; i++)
-		swap(mesh->faces[i][0], mesh->faces[i][2]);
-	dprintf("Done.\n");
+  for (int i = 0; i < nf; i++)
+    swap(mesh->faces[i][0], mesh->faces[i][2]);
+  dprintf("Done.\n");
 
-	if (had_tstrips)
-		mesh->need_tstrips();
+  if (had_tstrips)
+    mesh->need_tstrips();
 }
 
 } // namespace trimesh
